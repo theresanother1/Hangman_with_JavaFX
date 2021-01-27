43e9eea4320f9f87c8ee638452bf6d194d58bd64
theresanother1 <there.sa@posteo.net>
2021-01-27 14:28:45 +0100

    Wörter in Datei ausgelagert.

--- src/main/java/at/ac/fhcampuswien/hangmanwithjfx/App.java
+++ src/main/java/at/ac/fhcampuswien/hangmanwithjfx/App.java
@@ -11,7 +11,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class App extends Application  {
@@ -19,6 +20,76 @@ public class App extends Application  {
 
     public static void main(String[] args) throws IOException {
         System.out.println("blubb");
+
+        /*
+
+        Skript zur Erstellung der Datei mit Wörtern drin
+
+        //https://www.daniweb.com/programming/software-development/threads/168224/hangman-java-console-mode
+        //https://www.hangmanwords.com/words
+        String[] dictionary = {"abstraction", "ambiguous", "arithmetic", "backslash", "bitmap", "circumstance", "combination", "cafebabe",
+                "consortium", "decrement", "dependency", "dynamic", "encapsulate", "equivalent", "expression", "facilitate", "micro",
+                "fragment", "decimal", "implement", "distinguish", "leon", "inheritance", "internet", "java", "localize",
+                "navigation", "optimization", "parameter", "patrick", "pickle", "polymorphic", "rigorously", "specific",
+                "structure", "lexical", "likewise", "management", "manipulate", "maths", "hotjava", "vertex", "unsigned", "traditional",
+                "girl", "mother", "father", "sister", "love", "wind", "water", "study", "ball",
+                "puppy", "kitten", "apple", "pear", "lemon", "mango", "peach", "apricot", "chips", "steak", "fries", "cheese",
+                "potato", "wedge", "heel", "hand", "foot", "nose", "face", "mouth", "tongue", "johannes", "sven", "fingers", "toes", "line", "space",
+                "phone", "cord", "core", "grass", "trees", "birds", "animals", "lazy", "funny", "king", "queen", "heart", "heat", "cold",
+                "moon", "movie", "theater", "hairy", "small", "large", "huge", "donkey", "chicken", "pizza", "bread", "stones",
+                "sticks", "leaves", "letters", "alphabet", "soup", "hungry", "juergen", "michael", "tired", "theresa", "sleepy", "noisy", "caring", "friends", "month", "light",
+                "toothbrush", "savings", "bank", "account", "teller", "paper", "pencil", "coffee", "spirit", "ghost", "melon", "necklace",
+                "screen", "balloon", "string", "calendar", "work", "toys", "kids", "school", "class", "campus", "freedom", "liberty", "happiness",
+                "university", "message", "marker", "crayon", "eraser", "music", "lyrics", "songs", "ballads", "shapes", "triangle", "circle", "rectangle",
+                "square", "oval", "show", "video", "player", "team", "sport", "basketball", "football", "soccer", "softball", "baseball", "tennis",
+                "hockey", "lacrosse", "volleyball", "circuit", "blade", "scratch", "home", "house", "safe", "safety", "number", "count", "bear",
+                "goose", "lama", "panda", "lion", "tiger", "cheetah", "computer", "crackers", "rice", "shoes", "book", "story", "princess",
+                "prince", "jester", "court", "jury", "judge", "bench", "scandal", "name", "newspaper", "sebastian", "press", "shove", "tear", "magic", "tricks",
+                "cereal", "breakfast", "lunch", "dinner", "main", "course", "fork", "spoon", "sara", "knife", "lamp", "desk", "bottle", "highlighter",
+                "medicine", "seven", "flower", "rose", "petal", "abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward", "axiom",
+                "azure", "bagpipes", "bandwagon", "banjo", "bayou", "beekeeper", "bikini", "blizzard", "boggle", "bookworm", "boxcar", "boxful",
+                "buckaroo", "buffalo", "buffoon", "buxom", "buzzard", "buzzing", "buzzwords", "caliph", "cobweb", "cockiness", "croquet", "crypt",
+                "curacao", "cycle", "daiquiri", "dirndl", "disavow", "dizzying", "duplex", "dwarves", "embezzle", "equip", "espionage", "exodus",
+                "faking", "fishhook", "fixable", "fjord", "flapjack", "flopping", "fluffiness", "flyby", "foxglove", "frazzled", "frizzled", "fuchsia",
+                "funny", "gabby", "galaxy", "galvanize", "gazebo", "gizmo", "glowworm", "glyph", "gnarly", "gnostic", "gossip", "grogginess",
+                "haiku", "haphazard", "hyphen", "iatrogenic", "icebox", "injury", "ivory", "jackpot", "jaundice", "jawbreaker", "jaywalk", "jazziest",
+                "jazzy", "jelly", "jigsaw", "jinx", "jiujitsu", "jockey", "jogging", "joking", "jovial", "joyful", "juicy", "jukebox", "jumbo", "kayak",
+                "kazoo", "keyhole", "khaki", "kilobyte", "kiosk", "kitsch", "kiwifruit", "klutz", "knapsack", "larynx", "lengths", "lucky", "luxury", "lymph",
+                "marquis", "matrix", "megahertz", "microwave", "mnemonic", "mystify", "naphtha", "nightclub", "nowadays", "idiot", "nymph", "onyx",
+                "ovary", "oxidize", "oxygen", "pajama", "peekaboo", "phlegm", "pixel", "pneumonia", "polka", "psyche", "puppy", "puzzling", "quartz", "queue",
+                "quiz", "quizzes", "quorum", "rhythm", "rickshaw", "schnapps", "scratch", "shiv", "snazzy", "sphinx", "spritz", "squawk", "staff", "strength",
+                "strengths", "stretch", "stronghold", "stymied", "subway", "swivel", "syndrome", "thriftless", "thumbscrew", "topaz", "transcript", "transgress",
+                "transplant", "twelfth", "unknown", "unworthy", "unzip", "uptown", "vaporize", "vixen", "vodka", "voodoo", "vortex", "voyeurism", "walkway",
+                "waltz", "wave", "wavy", "waxy", "wellspring", "wheezy", "whiskey", "whizzing", "whomever", "wimpy", "witchcraft", "wizard", "woozy", "wristwatch",
+                "wyvern", "xylophone", "yachtsman", "yippee", "yoked", "youthful", "yummy", "zephyr", "zigzag", "zigzagging", "zilch", "zipper", "zodiac", "zombie"};
+
+
+
+        Path myPath = Paths.get(".././Hangman_with_JavaFX/src/main/resources/Dictionary.txt");
+        //Erstellt das File am PC, falls es noch nicht vorhanden ist.
+        //1x als Skript ausführen, dann löschen, weil Datei ja dann im Projekt drinnen ist?
+
+        try {
+            //Checkt, ob die Datei schon vorhanden ist.
+            if (new File(String.valueOf(myPath)).isFile()) {
+                System.out.println("File already exists.");
+            }
+            //Wenn nicht, wird die Datei erstellt und Inhalt des Arrays hineingeschrieben.
+            else {
+                FileWriter writeIt = new FileWriter(new File(String.valueOf(myPath)));
+                for (String s : dictionary) {
+                    writeIt.write(s + "\n");
+                }
+                writeIt.close();
+            }
+        } catch (IOException e) {
+            System.out.println("File writing or creating Error.");
+            e.printStackTrace();
+        }
+
+         */
+
+
         launch(args);
     }
 
--- src/main/java/at/ac/fhcampuswien/hangmanwithjfx/game/Gameplay.java
+++ src/main/java/at/ac/fhcampuswien/hangmanwithjfx/game/Gameplay.java
@@ -2,6 +2,9 @@ package at.ac.fhcampuswien.hangmanwithjfx.game;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -37,49 +40,34 @@ public class Gameplay {
         lines();
     }
 
-    //Wählt das Wort aus String-Array aus.
+    //Wählt das Wort aus der Datei Dictionary aus.
     public String randomWord(){
-        //https://www.daniweb.com/programming/software-development/threads/168224/hangman-java-console-mode
-        //https://www.hangmanwords.com/words
-        String[] dictionary = {"abstraction", "ambiguous", "arithmetic", "backslash", "bitmap", "circumstance", "combination", "cafebabe",
-                        "consortium", "decrement", "dependency", "dynamic", "encapsulate", "equivalent", "expression", "facilitate", "micro",
-                        "fragment", "decimal", "implement", "distinguish", "leon", "inheritance", "internet", "java", "localize",
-                        "navigation", "optimization", "parameter", "patrick", "pickle", "polymorphic", "rigorously", "specific",
-                        "structure", "lexical", "likewise", "management", "manipulate", "maths", "hotjava", "vertex", "unsigned", "traditional",
-                        "girl", "mother", "father", "sister", "love", "wind", "water", "study", "ball",
-                        "puppy", "kitten", "apple", "pear", "lemon", "mango", "peach", "apricot", "chips", "steak", "fries", "cheese",
-                        "potato", "wedge", "heel", "hand", "foot", "nose", "face", "mouth", "tongue", "johannes", "sven", "fingers", "toes", "line", "space",
-                        "phone", "cord", "core", "grass", "trees", "birds", "animals", "lazy", "funny", "king", "queen", "heart", "heat", "cold",
-                        "moon", "movie", "theater", "hairy", "small", "large", "huge", "donkey", "chicken", "pizza", "bread", "stones",
-                        "sticks", "leaves", "letters", "alphabet", "soup", "hungry", "juergen", "michael", "tired", "theresa", "sleepy", "noisy", "caring", "friends", "month", "light",
-                        "toothbrush", "savings", "bank", "account", "teller", "paper", "pencil", "coffee", "spirit", "ghost", "melon", "necklace",
-                        "screen", "balloon", "string", "calendar", "work", "toys", "kids", "school", "class", "campus", "freedom", "liberty", "happiness",
-                        "university", "message", "marker", "crayon", "eraser", "music", "lyrics", "songs", "ballads", "shapes", "triangle", "circle", "rectangle",
-                        "square", "oval", "show", "video", "player", "team", "sport", "basketball", "football", "soccer", "softball", "baseball", "tennis",
-                        "hockey", "lacrosse", "volleyball", "circuit", "blade", "scratch", "home", "house", "safe", "safety", "number", "count", "bear",
-                        "goose", "lama", "panda", "lion", "tiger", "cheetah", "computer", "crackers", "rice", "shoes", "book", "story", "princess",
-                        "prince", "jester", "court", "jury", "judge", "bench", "scandal", "name", "newspaper", "sebastian", "press", "shove", "tear", "magic", "tricks",
-                        "cereal", "breakfast", "lunch", "dinner", "main", "course", "fork", "spoon", "sara", "knife", "lamp", "desk", "bottle", "highlighter",
-                        "medicine", "seven", "flower", "rose", "petal", "abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward", "axiom",
-                        "azure", "bagpipes", "bandwagon", "banjo", "bayou", "beekeeper", "bikini", "blizzard", "boggle", "bookworm", "boxcar", "boxful",
-                        "buckaroo", "buffalo", "buffoon", "buxom", "buzzard", "buzzing", "buzzwords", "caliph", "cobweb", "cockiness", "croquet", "crypt",
-                        "curacao", "cycle", "daiquiri", "dirndl", "disavow", "dizzying", "duplex", "dwarves", "embezzle", "equip", "espionage", "exodus",
-                        "faking", "fishhook", "fixable", "fjord", "flapjack", "flopping", "fluffiness", "flyby", "foxglove", "frazzled", "frizzled", "fuchsia",
-                        "funny", "gabby", "galaxy", "galvanize", "gazebo", "gizmo", "glowworm", "glyph", "gnarly", "gnostic", "gossip", "grogginess",
-                        "haiku", "haphazard", "hyphen", "iatrogenic", "icebox", "injury", "ivory", "jackpot", "jaundice", "jawbreaker", "jaywalk", "jazziest",
-                        "jazzy", "jelly", "jigsaw", "jinx", "jiujitsu", "jockey", "jogging", "joking", "jovial", "joyful", "juicy", "jukebox", "jumbo", "kayak",
-                        "kazoo", "keyhole", "khaki", "kilobyte", "kiosk", "kitsch", "kiwifruit", "klutz", "knapsack", "larynx", "lengths", "lucky", "luxury", "lymph",
-                        "marquis", "matrix", "megahertz", "microwave", "mnemonic", "mystify", "naphtha", "nightclub", "nowadays", "idiot", "nymph", "onyx",
-                        "ovary", "oxidize", "oxygen", "pajama", "peekaboo", "phlegm", "pixel", "pneumonia", "polka", "psyche", "puppy", "puzzling", "quartz", "queue",
-                        "quiz", "quizzes", "quorum", "rhythm", "rickshaw", "schnapps", "scratch", "shiv", "snazzy", "sphinx", "spritz", "squawk", "staff", "strength",
-                        "strengths", "stretch", "stronghold", "stymied", "subway", "swivel", "syndrome", "thriftless", "thumbscrew", "topaz", "transcript", "transgress",
-                        "transplant", "twelfth", "unknown", "unworthy", "unzip", "uptown", "vaporize", "vixen", "vodka", "voodoo", "vortex", "voyeurism", "walkway",
-                        "waltz", "wave", "wavy", "waxy", "wellspring", "wheezy", "whiskey", "whizzing", "whomever", "wimpy", "witchcraft", "wizard", "woozy", "wristwatch",
-                        "wyvern", "xylophone", "yachtsman", "yippee", "yoked", "youthful", "yummy", "zephyr", "zigzag", "zigzagging", "zilch", "zipper", "zodiac", "zombie"};
 
+        //Zählt die Zeilenanzahl in der Datei. Sollte erhalten bleiben, falls Datei ergänzt oder verkleinert wird.
+        long linesInFile = 0;
+        Path myPath = Paths.get(".././Hangman_with_JavaFX/src/main/resources/Dictionary.txt");
+        try {
+            linesInFile = Files.lines(myPath).count();
+            System.out.println("Counted lines in file.");
+        } catch (IOException e){
+            System.out.println("Troubles counting lines.");
+            e.printStackTrace();
+        }
+        System.out.println(linesInFile);
+
+        //Random intialisieren, um eine zufällige Zeile zu ermitteln.
         Random random = new Random();
-        int index = random.nextInt(dictionary.length);
-        this.wordToFind = dictionary[index];
+        int index = random.nextInt((int) linesInFile);
+
+        //Ordnet dem String specificWord das Wort aus der zufällig ermittelten Zeile zu.
+        try {
+            this.wordToFind = Files.readAllLines(myPath).get(index);
+            System.out.println(wordToFind);
+            System.out.println("read the word from the file.");
+        } catch (IOException f) {
+            System.out.println("File reading Error. ");
+            f.printStackTrace();
+        }
         return wordToFind;
     }
 
